7abf47096593b3b9850318ce4eb76da2ec237e2c
Exterra <choi3377@naver.com>
2016-11-23 02:43:25 +0900

    20161123 first commit

--- chap02/Project_1/app/src/main/java/com/example/exterra/project_1/MainActivity.java
+++ chap02/Project_1/app/src/main/java/com/example/exterra/project_1/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.exterra.project_1;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,18 +20,24 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // 해상도 가로 세로 구하는 코드
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         deviceWidth = displayMetrics.widthPixels;
         deviceHeight = displayMetrics.heightPixels;
 
-        //리니어 레이아웃
+        // 리니어 레이아웃
         LinearLayout layout = new LinearLayout(this);
         layout.setBackgroundColor(Color.YELLOW);
         layout.setOrientation(LinearLayout.VERTICAL); // 세로로
 
+        // 앞에서 만들었던 리니어 레이아웃 설정하기
         setContentView(layout);
-        textView01(layout);
+
+        // textView01 함수로 빼서 사용, layout을 매개변수로 하여
+        // 해당 TextView를 이 레이아웃에 추가하는 역할을 하게 함.
+//       textView01(layout);
+        imageView01(layout);
     }
 
     public void textView01(LinearLayout layout){
@@ -39,8 +49,19 @@ public class MainActivity extends Activity {
         tv1.setLayoutParams(new LinearLayout.LayoutParams
                 (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 0.0f));
         // 부모의 객체와 가로와 세로의 크기를 같게 만든다 ->
-        // 중앙 정렬의 의미를 부모 화면에서의 실행을 위해 MATCH_PARENT로 지정하였다.
+        // 중앙 정렬의 의미를 부모 화면에서의 실행을 위해 MATdCH_PARENT로 지정하였다.
 
         layout.addView(tv1); // 찍어준다.
+    }
+
+    public void imageView01(LinearLayout layout){
+        ImageView rabbit = new ImageView(this);
+
+        rabbit.setImageResource(R.drawable.rabbit);
+        rabbit.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        rabbit.setLayoutParams(new LinearLayout.LayoutParams
+                (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 0.0f));
+
+        layout.addView(rabbit);
     }
 }
